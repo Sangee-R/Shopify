d062b7acf099521f70992d9fa6785713303637c7
Sangee-R <sangeethar0205@gmail.com>
2022-11-17 23:04:19 +0530

    shopping

---
+++ src/data.js
@@ -0,0 +1,148 @@
+const productdata = [
+ {
+  "productName": "PUMA Colorblock Men Track Suit",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/xif0q/track-suit/n/p/5/xs-606007-puma-original-imaghxxnrekz9yvw.jpeg?q=70",
+  "productPrice": "950",
+  "productRating": 3.3,
+  "sale": true,
+  "productSalePrice": "201.00",
+  "id": "1"
+ },
+ {
+  "productName": "Colorblock Men Track Suit",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/ksez24w0/track-suit/k/2/y/xxl-mens-tracksuit-b-31-grey-rakshita-collection-original-imag5zhthegtqmw5.jpeg?q=70",
+  "productPrice": "879",
+  "productRating": 3.6,
+  "sale": false,
+  "productSalePrice": "66.00",
+  "id": "2"
+ },
+ {
+  "productName": "Printed, Floral Print Daily Wear Georgette Saree  (Pack of 3, Blue, Pink, Grey)",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/kynb6vk0/sari/0/v/v/free-1955s350-1926s306-1850s178-samah-unstitched-original-imagau5sf2cgch9t.jpeg?q=70",
+  "productPrice": "599",
+  "productRating": 3.8,
+  "sale": false,
+  "productSalePrice": "785.00",
+  "id": "3"
+ },
+ {
+  "productName": "Woven Bollywood Jacquard Saree  (Pink, Cream)",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/k7c88sw0/sari/j/z/y/free-ladli-krishna-r-fashion-original-imafphxuxmkutnt8.jpeg?q=70",
+  "productPrice": "419.00",
+  "productRating": 4,
+  "sale": false,
+  "productSalePrice": "144.00",
+  "id": "4"
+ },
+ {
+  "productName": "Women Printed Viscose Rayon Anarkali Kurta  (Pink)",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/xif0q/kurta/d/f/r/xl-1005-amsr-fashion-original-imagjvn8ytbfsmuy.jpeg?q=70",
+  "productPrice": "426",
+  "productRating": 3.9,
+  "sale": true,
+  "productSalePrice": "582.00",
+  "id": "5"
+ },
+ {
+  "productName": "Women Embroidered Viscose Rayon Straight Kurta  (Blue)",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/l5e81ow0/kurta/1/k/f/s-fasho-101-aaysa-original-imagg2pgujftfkzr.jpeg?q=70",
+  "productPrice": "399",
+  "productRating": 4.2,
+  "sale": true,
+  "productSalePrice": "675.00",
+  "id": "6"
+ },
+ {
+  "productName": "Floral Print Semi Stitched Lehenga Choli  (Blue)",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/kt7jv680/lehenga-choli/f/n/i/free-sleeveless-l8097-fashionuma-original-imag6hxffsx8hkgh.jpeg?q=70",
+  "productPrice": "618",
+  "productRating": 4.2,
+  "sale": false,
+  "productSalePrice": "186.00",
+  "id": "7"
+ },
+ {
+  "productName": "Embroidered Semi Stitched Lehenga Choli  (Red)",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/jzk5xu80/lehenga-choli/u/x/9/free-p0001-skytara-original-imafjhnhp7anhce8.jpeg?q=70",
+  "productPrice": "429",
+  "productRating": 3.9,
+  "sale": true,
+  "productSalePrice": "854.00",
+  "id": "8"
+ },
+ {
+  "productName": "Women Kurta and Pant Set Viscose Rayon",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/l20rma80/ethnic-set/m/m/9/xxl-131-wine-kurta-pant-dupatta-set-ziva-fashion-original-imagdgbpyzftg5va.jpeg?q=70",
+  "productPrice": "729",
+  "productRating": 4.2,
+  "sale": false,
+  "productSalePrice": "612.00",
+  "id": "9"
+ },
+ {
+  "productName": "Women Kurta and Palazzo Set Art Silk",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/l2w7b0w0/ethnic-set/9/7/9/xxl-5048-v-tradition-original-image5fpzydceh83.jpeg?q=70",
+  "productPrice": "599",
+  "productRating": 4.5,
+  "sale": false,
+  "productSalePrice": "646.00",
+  "id": "10"
+ },
+ {
+  "productName": "Boys Festive & Party Shirt & Dhoti Set  (Dark Blue Pack of 1)",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/l31x2fk0/kids-ethnic-set/p/w/a/6-12-months-ds-ramar-blue-kids-stylish-original-imag9hhm4znapgyr.jpeg?q=70",
+  "productPrice": "380",
+  "productRating": 4.2,
+  "sale": false,
+  "productSalePrice": "130.00",
+  "id": "11"
+ },
+ {
+  "productName": "Boys Festive & Party Kurta, Dhoti Pant & Dupatta Set  (Gold Pack of 1)",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/xif0q/kids-ethnic-set/6/z/s/3-4-years-tdk-21-ftc-fashions-original-imaf237gbqpb9gwg-bb.jpeg?q=70",
+  "productPrice": "203",
+  "productRating": 3.9,
+  "sale": true,
+  "productSalePrice": "526.00",
+  "id": "12"
+ },
+ {
+  "productName": "Girls Below Knee Festive/Wedding Dress  (Pink, 3/4 Sleeve)",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/xif0q/kids-dress/f/w/v/6-7-years-skirt-top-pink-zonafashion-original-imagbkx9x2qwbf8r-bb.jpeg?q=70",
+  "productPrice": "355",
+  "productRating": 3.9,
+  "sale": true,
+  "productSalePrice": "902.00",
+  "id": "13"
+ },
+ {
+  "productName": "Girls Calf Length Casual Dress  (Gold, 3/4 Sleeve)",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/kxgfzbk0/kids-dress/o/w/t/12-13-years-dezine-yellow-black-flower-gown-kaarigari-original-imag9wx77mzfyezn.jpeg?q=70",
+  "productPrice": "172",
+  "productRating": 3.8,
+  "sale": false,
+  "productSalePrice": "247.00",
+  "id": "14"
+ },
+ {
+  "productName": "Velcro Sneakers For Boys & Girls  (Red)",
+  "productImg": "https://rukminim1.flixcart.com/image/832/832/xif0q/kids-shoe/9/v/i/-original-imagg7k4zpzd99vf.jpeg?q=70",
+  "productPrice": "249",
+  "productRating": 4,
+  "sale": false,
+  "productSalePrice": "188.00",
+  "id": "15"
+ },
+ {
+    "productName": "Lace Sneakers For Boys & Girls  (Blue)",
+    "productImg": "https://rukminim1.flixcart.com/image/832/832/l48s9zk0/kids-shoe/p/3/6/3-390467-puma-original-imagf6n43rbyhh2b.jpeg?q=70",
+    "productPrice": "999",
+    "productRating": 4.5,
+    "sale": true,
+    "productSalePrice": "200.00",
+    "id": "16"
+   }
+];
+
+export default productdata;
